58dac268fc10c53712dfef2506735da5d33ff5dc
ErtaKryeziu <ek67377@ubt-uni.net>
2024-04-22 03:03:12 +0200

    Update User Functionality

--- api/routes/User.js
+++ api/routes/User.js
@@ -1,27 +1,22 @@
 // Set path for the .env file which contains the key (and other variables in the future maybe?).
 // Create a file.env in the root dir, /api/file.env, then add the path as an object inside the config param like
 // require('dotenv').config({path: 'C:/User/Desktop/audit/api/file.env'}), and store the var as SECRET_KEY=randomvaluehere (only for testing)
-require('dotenv').config({ path: "C:/Users/ccc/OneDrive/Desktop/audit/api/var.env " });
+require('dotenv').config({path: 'C:/Users/hp/OneDrive/Desktop/audit/api/file.env' });
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-const { User } = require('../models');
-const verifyToken = require('../middlewares/authMiddleware');
+const { User } = require('../models/Users');
+// const verifyToken = require('../middlewares/authMiddleware');
 
 // JWT Secret Key (Stored in .env file inside the root dir)
 const secretKey = process.env.SECRET_KEY;
-
-
-
 
 router.get('/', async (req, res) => {
     const listAllUsers = await User.findAll();
     res.json(listAllUsers);
 });
-
-
 
 // Register User API Endpoint
 router.post('/register', async (req, res) => {
@@ -43,6 +38,33 @@ router.post('/register', async (req, res) => {
     }
 });
 
+
+
+router.post('/users/:id', async (req, res, next) => {
+   const userId = req.body.id;
+   const { email, username, role } = req.body;
+    
+   try {
+    const user = await User.findOne({where: {userId} });
+
+    if(!user){
+        return res.status(404).json({ error: 'This user does not exist'});
+    }
+
+    user.email = email;
+    user.username = username;
+    user.role = role;
+
+    const updatedUser = await user.save();
+
+    res.status(200).json(updatedUser);
+
+   } catch (error) {
+    console.error('Error in updating this user', error);
+    res.status(500).json({error: 'Internal Server Error'});
+   }
+ 
+});
 
 // User Login API Endpoint
 router.post('/login', async(req, res) => {
